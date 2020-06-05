b1017a02fe80ab7b447849ae1b30df5ea08cb547
Thiagomrfs <thiagomarbsn@gmail.com>
2020-06-05 20:25:56 -0300

    added backend integration

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ const db = require("./database/db.js")
 // configurar pasta publica
 server.use(express.static("public"))
 
+// habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true}))
+
 // template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/view", {
@@ -24,6 +27,42 @@ server.get("/", (req, res) => {
 
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+    // query strings da url
+    // const userQuery = req.query
+    const data = req.body
+
+    const query = ` 
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);`
+    const values = [
+        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
+        "Colectoria",
+        "Guilherme Gemballa",
+        "Nº 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos, Lâmpadas"
+    ]
+    
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+        }
+
+        console.log("cadastrado com sucesso")
+        console.log(this)
+    })
+
 })
 
 server.get("/search-results", (req, res) => {
